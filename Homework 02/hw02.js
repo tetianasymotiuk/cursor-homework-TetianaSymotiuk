9db5f117979d8fe3040bf2ee54e1143fbfcd5e19
Tetiana <tetianaprivat@gmail.com>
2020-02-03 15:52:55 +0200

    competed HW02

--- Homework 02/hw02.js
+++ Homework 02/hw02.js
@@ -1,8 +1,8 @@
-let startN = parseInt(prompt("Уведіть число N"), 10); //чи ціле число
+let startN = parseInt(prompt("Уведіть число N"), 10); //переведення з string в numberчи ціле число
 
 console.log( "Number N : ",  startN);
 
-console.log(Number.isInteger(startN)); //переведення з string в number
+console.log(Number.isInteger(startN)); //чи ціле число
 
 let finishM = parseInt(prompt("Уведіть число M"), 10);
 
@@ -13,3 +13,25 @@ console.log(Number.isInteger(finishM));
 let answer = confirm("Пропускати парні числа?");
 console.log("answer : ", answer);
 
+let sum = 0;
+
+if(answer) {
+    for (let i=startN; i<=finishM; i++) {
+        if (i%2===0) {
+            continue;
+        }else {sum = sum + i;
+        }
+    }
+    console.log("Сума чисел з парними числами: " + (sum));
+    result=`Сума чисел з парними числами: ${sum};
+    `;
+}else{
+    for (let a=startN; a<=finishM; a++) {
+        sum = sum + a;
+    }
+    console.log("Сума чисел: " + (sum));
+    result = `Сума чисел: ${sum};
+    `;
+};
+
+document.getElementById("div1").innerHTML = result;
