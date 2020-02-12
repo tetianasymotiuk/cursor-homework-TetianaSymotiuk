ff65f9f6e17f2db7b0cc2fb3543d10ca10918ec9
Tetiana <tetianaprivat@gmail.com>
2020-02-12 11:21:49 +0200

    added check

--- Homework 02/hw02.js
+++ Homework 02/hw02.js
@@ -1,37 +1,34 @@
-let startN = parseInt(prompt("Введіть число N"), 10); //переведення з string в numberчи ціле число
+let startN = Math.round(prompt("Введіть число N:"));
+let finishM = Math.round(prompt("Введіть число M:"));
 
-console.log( "Number N : ",  startN);
-
-console.log(Number.isInteger(startN)); //чи ціле число
-
-let finishM = parseInt(prompt("Введіть число M"), 10);
-
-console.log( "Number M : ", finishM);
-
-console.log(Number.isInteger(finishM));
-
-let answer = confirm("Пропускати парні числа?");
-console.log("answer : ", answer);
-
-let sum = 0;
-
-if(answer) {
-    for (let i=startN; i<=finishM; i++) {
-        if (i%2===0) {
-            continue;
-        }else {sum = sum + i;
-        }
-    }
-    console.log("Сума чисел з парними числами: " + (sum));
-    result=`Сума чисел з парними числами: ${sum};
-    `;
-}else{
-    for (let a=startN; a<=finishM; a++) {
-        sum = sum + a;
-    }
-    console.log("Сума чисел: " + (sum));
-    result = `Сума чисел: ${sum};
-    `;
-};
+if(startN == "" || startN !== parseInt(startN) || finishM == "" || finishM !== parseInt(finishM)) {
+    alert("Номер введено не вірно!")
+        } else { 
+            let answer = confirm("Пропускати парні числа?");
+            let sum = 0;
+            let result = ``;
+            if(answer){
+                    for (let i=startN; i<=finishM; i++) {
+                        if (i%2===0) {
+                            continue;
+                        } else {
+                            sum = sum + i;
+                        }
+                    }
+                    console.log("Сума чисел з парними числами: " + (sum));
+                    result=`Сума чисел з парними числами: ${sum};
+                    `;
+                } else {
+                    for (let a=startN; a<=finishM; a++) {
+                        sum = sum + a;
+                    }
+                    console.log( "Number M : ", finishM);
+                    console.log( "Number N : ",  startN);
+                    console.log("Сума чисел: " + (sum));
+                    result = `Сума чисел: ${sum};
+                    `;
+            };
 
 document.getElementById("div1").innerHTML = result;
+}
+
